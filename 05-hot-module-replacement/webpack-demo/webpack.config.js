f4e248d57bec54da707a65cb43fa4077f88de01f
guyimin <1205738549@qq.com>
2017-09-18 21:26:38 +0800

    feat(05-hot-module-replacement): enable HMR

--- 05-hot-module-replacement/webpack-demo/webpack.config.js
+++ 05-hot-module-replacement/webpack-demo/webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   devtool: 'inline-source-map',
-  // 尝试webpack-dev-middleware，先注释掉webpack-dev-server模块相关控制
-  // devServer: {
-  //   contentBase: './dist'
-  // },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Development'
-    })
+      title: 'Hot Module Replacement'
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    // 下面这行是配置webpack-dev-middleware
-    publicPath: '/'
   }
 }
