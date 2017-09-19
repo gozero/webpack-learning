079ebc2b2a9ff6bd8c587550c7d6585fcad68014
guyimin <1205738549@qq.com>
2017-09-19 11:07:37 +0800

    style: reset tabsize and format codes

--- 02-asset-management/webpack-demo/src/index.js
+++ 02-asset-management/webpack-demo/src/index.js
@@ -4,21 +4,21 @@ import Icon from './icon.png'
 import Data from './data.xml'
 
 function component() {
-    var element = document.createElement('div');
+  var element = document.createElement('div');
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
 
-    // 将图像添加到我们现有的 div。
-    var myIcon = new Image()
-    myIcon.src = Icon
+  // 将图像添加到我们现有的 div。
+  var myIcon = new Image()
+  myIcon.src = Icon
 
-    element.appendChild(myIcon)
+  element.appendChild(myIcon)
 
-    console.log(Data)
+  console.log(Data)
 
-    return element
+  return element
 }
 
 document.body.appendChild(component())
--- 06-tree-shaking/webpack-demo/src/index.js
+++ 06-tree-shaking/webpack-demo/src/index.js
@@ -4,13 +4,13 @@ import { cube } from './math.js'
 import './styles.css'
 
 function component() {
-    var element = document.createElement('pre')
-    element.innerHTML = [
-        'Hello webpack!',
-        '5 cubed is equal to ' + cube(5)
-    ].join('\n\n')
+  var element = document.createElement('pre')
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n')
 
-    return element
+  return element
 }
 
 document.body.appendChild(component())
